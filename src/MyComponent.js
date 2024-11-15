9f93e3bb4fe5f3677abadd0dcd6083cfa986d755
Javier Gomez <javier.gov@outlook.es>
2024-11-15 16:22:29 -0600

    Creacion de primer componente

---
+++ src/MyComponent.js
@@ -0,0 +1,12 @@
+// Importar modulos o dependencias
+import React from "react";
+
+// funcion
+const MyComponent = () => {
+    return <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo pariatur non, et porro aperiam quisquam dolorum tempora omnis aliquid reprehenderit quaerat beatae vitae, ut nostrum, labore natus vel quos temporibus?</p>
+    
+}
+
+
+// Export
+export default MyComponent

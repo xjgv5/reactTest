e4c8c5410fb980bf68aeebb3d9b4899f1cce6b41
Javier Gomez <javier.gov@outlook.es>
2024-11-20 20:51:00 -0600

    evento doble click

--- src/EventosComponentes.js
+++ src/EventosComponentes.js
@@ -7,12 +7,22 @@ export const EventosComponentes = () => {
         alert("has dado click al boton" + nombre)
     }
 
+    function dobleClick(){
+        alert("Has dado doble click")
+    }
+
   return (
-    <div>
+    <div className='EventoComponenteContainer'>
         <hr />
         <h1>Eventos en react</h1>
-        {/* Evento click */}
-        <button className='clickBtn' onClick={e => handleClick(e, 'Xavier')}>Click</button>
+        
+        <div className="button__container">
+            {/* Evento click */}
+            <button className='clickBtn' onClick={e => handleClick(e, 'Xavier')}>Click</button>
+
+            {/* Evento doble click */}
+            <button className='clickBtn' onDoubleClick={dobleClick}>Doble click</button>
+        </div>
 
     </div>
   )
